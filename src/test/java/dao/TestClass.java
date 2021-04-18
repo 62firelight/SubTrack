9de601e582b51e974ddc93e6a3b11f95851c6b84
Luke Tang <tanlu824@student.otago.ac.nz>
2021-04-18 19:59:11 +1200

    Added test class template
    References #5.

---
+++ src/test/java/dao/TestClass.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import domain.Subscription;
+import java.math.BigDecimal;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author Luke Tang
+ */
+public class TestClass {
+
+    @BeforeEach
+    public void setUp() {
+        
+    }
+
+    @AfterEach
+    public void tearDown() {
+        
+    }
+
+    @Test
+    public void testSomething() {
+        assertThat(2 + 2, equalTo(4));
+    }
+    
+}
