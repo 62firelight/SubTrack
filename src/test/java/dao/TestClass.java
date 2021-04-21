50d298f236ee480c01b040d5e32be58d2123d7a8
trbay <trbay@LAPTOP-AFK46AGF>
2021-04-22 09:59:09 +1200

    Completion of testing for saving customer to dao and getting customer from dao based off their username

--- src/test/java/dao/TestClass.java
+++ src/test/java/dao/TestClass.java
@@ -27,16 +27,17 @@ import org.junit.jupiter.api.Test;
  * @author Luke Tang
  */
 public class TestClass {
-    //private CustomerCollectionsDAO CustDAO = new CustomerCollectionsDAO();
+    //Format code below doesnt seem to allow the test to run on my computer
+    /*private CustomerCollectionsDAO CustDAO = new CustomerCollectionsDAO();
     //private Customer cust1 = new Customer();
-    //private Customer cust2 = new Customer();
+    private Customer cust2 = new Customer();*/
 
     @BeforeEach
     public void setUp() {
         CustomerCollectionsDAO CustDAO = new CustomerCollectionsDAO();
         Customer cust1 = new Customer();
         Customer cust2 = new Customer();
-
+        
         Subscription sub1 = new Subscription();
         sub1.setName("Netflix");
         sub1.setSubscriptionId(123);
@@ -45,7 +46,7 @@ public class TestClass {
         sub1.setSubscriptionPrice(BigDecimal.TEN);
         sub1.setDescription("Movies and TV");
         sub1.setCompanyName("Netflix Inc.");
-
+        //This customer setup becomes redundant due to my set up
         cust1.setFirstName("Taine");
         cust1.setLastName("Bayly");
         cust1.setUsername("bayta267");
@@ -53,18 +54,19 @@ public class TestClass {
         cust1.setPhoneNumber("0273842");
         cust1.setEmailAddress("bayta@student.com");
         cust1.setCustomerId(1);
-        
-        CustDAO.saveCustomer(cust1);
-    }
 
+        //CustDAO.saveCustomer(cust1);
+    }
+    
     @AfterEach
     public void tearDown() {
-
+        
     }
-
+    
     @Test
     public void testSaveCustomer() {
-        //CustDAO.saveCustomer(cust1);
+        
+        //Weird set up in test method due to errors I was having using setup.
         Customer cust1 = new Customer();
         cust1.setFirstName("Taine");
         cust1.setLastName("Bayly");
@@ -83,13 +85,31 @@ public class TestClass {
         cust2.setEmailAddress("tudent.com");
         cust2.setCustomerId(2);
         
-        
         CustomerCollectionsDAO CustDAO = new CustomerCollectionsDAO();
         CustDAO.saveCustomer(cust1);
-        //CustomerCollectionsDAO CustDAO = new CustomerCollectionsDAO();
-        Customer r = CustDAO.getCustomer("bayta267");
-        assertEquals("Retreived customer should be the same", cust1,r);
-        assertThat(1 + 1, equalTo(2));
-    }
 
+        //Use of assert equals to check that get customer method, actually retrieves customer 1 from list based of username.
+        //This test also confirms our save function is working successfully
+        Customer retrieved = CustDAO.getCustomer("bayta267");
+        
+        assertEquals("Retreived customer should be the same", cust1, retrieved);
+        
+        
+        /**assertTrue("cust1 should exist", CustDAO.contains(cust1));*/
+        
+        //Follow up testing of save and get function testing manual attributes
+        CustDAO.saveCustomer(cust2);
+        Customer secondRetreieved = CustDAO.getCustomer("a267");
+        
+        //Manual checks of each function
+        assertEquals(cust2.getFirstName(), secondRetreieved.getFirstName());
+        assertEquals(cust2.getLastName(), secondRetreieved.getLastName());
+        assertEquals(cust2.getUsername(), secondRetreieved.getUsername());
+        assertEquals(cust2.getPassword(), secondRetreieved.getPassword());
+        assertEquals(cust2.getPhoneNumber(), secondRetreieved.getPhoneNumber());
+        assertEquals(cust2.getEmailAddress(), secondRetreieved.getEmailAddress());
+        assertEquals(cust2.getCustomerId(), secondRetreieved.getCustomerId());
+        
+    }
+    
 }
