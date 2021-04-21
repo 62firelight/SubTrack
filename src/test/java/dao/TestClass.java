0cab6b7e8f63edeaeec50816041cdc3adf98a81e
trbay <trbay@LAPTOP-AFK46AGF>
2021-04-22 10:28:55 +1200

    inclusion of subscription save and get function testing. Need to discuss with Luke potential Bug.

--- src/test/java/dao/TestClass.java
+++ src/test/java/dao/TestClass.java
@@ -8,16 +8,7 @@ package dao;
 import domain.Customer;
 import domain.Subscription;
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 import static junit.framework.Assert.assertEquals;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.hasItem;
-import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.CoreMatchers.nullValue;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.hasProperty;
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -27,6 +18,7 @@ import org.junit.jupiter.api.Test;
  * @author Luke Tang
  */
 public class TestClass {
+
     //Format code below doesnt seem to allow the test to run on my computer
     /*private CustomerCollectionsDAO CustDAO = new CustomerCollectionsDAO();
     //private Customer cust1 = new Customer();
@@ -37,7 +29,7 @@ public class TestClass {
         CustomerCollectionsDAO CustDAO = new CustomerCollectionsDAO();
         Customer cust1 = new Customer();
         Customer cust2 = new Customer();
-        
+
         Subscription sub1 = new Subscription();
         sub1.setName("Netflix");
         sub1.setSubscriptionId(123);
@@ -57,15 +49,15 @@ public class TestClass {
 
         //CustDAO.saveCustomer(cust1);
     }
-    
+
     @AfterEach
     public void tearDown() {
-        
+
     }
-    
+
     @Test
     public void testSaveCustomer() {
-        
+
         //Weird set up in test method due to errors I was having using setup.
         Customer cust1 = new Customer();
         cust1.setFirstName("Taine");
@@ -75,7 +67,7 @@ public class TestClass {
         cust1.setPhoneNumber("0273842");
         cust1.setEmailAddress("bayta@student.com");
         cust1.setCustomerId(1);
-        
+
         Customer cust2 = new Customer();
         cust2.setFirstName("ne");
         cust2.setLastName("ly");
@@ -84,23 +76,23 @@ public class TestClass {
         cust2.setPhoneNumber("042");
         cust2.setEmailAddress("tudent.com");
         cust2.setCustomerId(2);
-        
+
         CustomerCollectionsDAO CustDAO = new CustomerCollectionsDAO();
         CustDAO.saveCustomer(cust1);
 
         //Use of assert equals to check that get customer method, actually retrieves customer 1 from list based of username.
         //This test also confirms our save function is working successfully
         Customer retrieved = CustDAO.getCustomer("bayta267");
-        
+
         assertEquals("Retreived customer should be the same", cust1, retrieved);
-        
-        
-        /**assertTrue("cust1 should exist", CustDAO.contains(cust1));*/
-        
+
+        /**
+         * assertTrue("cust1 should exist", CustDAO.contains(cust1));
+         */
         //Follow up testing of save and get function testing manual attributes
         CustDAO.saveCustomer(cust2);
         Customer secondRetreieved = CustDAO.getCustomer("a267");
-        
+
         //Manual checks of each function
         assertEquals(cust2.getFirstName(), secondRetreieved.getFirstName());
         assertEquals(cust2.getLastName(), secondRetreieved.getLastName());
@@ -109,7 +101,56 @@ public class TestClass {
         assertEquals(cust2.getPhoneNumber(), secondRetreieved.getPhoneNumber());
         assertEquals(cust2.getEmailAddress(), secondRetreieved.getEmailAddress());
         assertEquals(cust2.getCustomerId(), secondRetreieved.getCustomerId());
-        
+
+        //credential validation
     }
-    
+
+    @Test
+    public void testSaveSubscription() {
+        Subscription sub1 = new Subscription();
+        sub1.setName("Netflix");
+        sub1.setSubscriptionId(123);
+        sub1.setPaid(false);
+        sub1.setCategory("Leisure");
+        sub1.setSubscriptionPrice(BigDecimal.TEN);
+        sub1.setDescription("Movies and TV");
+        sub1.setCompanyName("Netflix Inc.");
+
+        Subscription sub2 = new Subscription();
+        sub2.setName("flix");
+        sub2.setSubscriptionId(1234);
+        sub2.setPaid(false);
+        sub2.setCategory("Lei");
+        sub2.setSubscriptionPrice(BigDecimal.TEN);
+        sub2.setDescription("TV");
+        sub2.setCompanyName("flix Inc.");
+
+        SubscriptionCollectionsDAO subDAO = new SubscriptionCollectionsDAO();
+
+        subDAO.saveSubscription("subtest1", sub1);
+
+        //below sits tests that should work if we adjust subscriptionDAO get method
+        /*
+        Subscription subRetrieved = subDAO.getSubscriptionsByUsername("Subtest1");
+        
+        
+        assertEquals("Retreived subscrpiton should be the same", sub1, subRetrieved);
+        
+        
+        //Manual checks of each function attribute success for sub2
+        subDAO.saveSubscription("subtest2",sub2);
+        
+        Subscription subRetrieved2 = subDAO.getSubscriptionsByUsername("Subtest2");
+        assertEquals(sub2.getName(), subRetrieved2.getName());
+        assertEquals(sub2.getSubscriptionId(), subRetrieved2.getgetSubscriptionId));
+        assertEquals(sub2.getPaid(), subRetrieved2.getPaid());
+         assertEquals(sub2.getCategory(), subRetrieved2.getCategory());
+        assertEquals(sub2.getSubscriptionPrice(), subRetrieved2.getSubscriptionPrice());
+        assertEquals(sub2.getDescription(), subRetrieved2.getDescription());
+        assertEquals(sub2.getCompanyName(), subRetrieved2.getCompanyName());
+        
+         */
+        // assertThat(1 + 1, equalTo(2));
+    }
+
 }
