da4ccb49283d4f34d714e9e762d395c6db186979
Luke <Luke@LAPTOP-GBPK7S6I.student.uod.otago.ac.nz.>
2021-04-23 14:11:26 +1200

    Emptied TestClass.java so it can be reused for other tests
    References #5.

--- src/test/java/dao/TestClass.java
+++ src/test/java/dao/TestClass.java
@@ -13,6 +13,7 @@ import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertTrue;
 import net.sf.oval.constraint.AssertTrueCheck;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.MatcherAssert.assertThat;
 import org.junit.jupiter.api.AfterEach;
@@ -25,35 +26,9 @@ import org.junit.jupiter.api.Test;
  */
 public class TestClass {
 
-    //Format code below doesnt seem to allow the test to run on my computer
-    /*private CustomerCollectionsDAO CustDAO = new CustomerCollectionsDAO();
-    //private Customer cust1 = new Customer();
-    private Customer cust2 = new Customer();*/
-
     @BeforeEach
     public void setUp() {
-        CustomerCollectionsDAO CustDAO = new CustomerCollectionsDAO();
-        Customer cust1 = new Customer();
-        Customer cust2 = new Customer();
-
-        Subscription sub1 = new Subscription();
-        sub1.setName("Netflix");
-        sub1.setSubscriptionId(123);
-        sub1.setPaid(false);
-        sub1.setCategory("Leisure");
-        sub1.setSubscriptionPrice(BigDecimal.TEN);
-        sub1.setDescription("Movies and TV");
-        sub1.setCompanyName("Netflix Inc.");
-        //This customer setup becomes redundant due to my set up
-        cust1.setFirstName("Taine");
-        cust1.setLastName("Bayly");
-        cust1.setUsername("bayta267");
-        cust1.setPassword("INFO310");
-        cust1.setPhoneNumber("0273842");
-        cust1.setEmailAddress("bayta@student.com");
-        cust1.setCustomerId(1);
-
-        //CustDAO.saveCustomer(cust1);
+        
     }
 
     @AfterEach
@@ -62,102 +37,8 @@ public class TestClass {
     }
 
     @Test
-    public void testSaveCustomer() {
-
-        //Weird set up in test method due to errors I was having using setup.
-        Customer cust1 = new Customer();
-        cust1.setFirstName("Taine");
-        cust1.setLastName("Bayly");
-        cust1.setUsername("bayta267");
-        cust1.setPassword("INFO310");
-        cust1.setPhoneNumber("0273842");
-        cust1.setEmailAddress("bayta@student.com");
-        cust1.setCustomerId(1);
-
-        Customer cust2 = new Customer();
-        cust2.setFirstName("ne");
-        cust2.setLastName("ly");
-        cust2.setUsername("a267");
-        cust2.setPassword("1234");
-        cust2.setPhoneNumber("042");
-        cust2.setEmailAddress("tudent.com");
-        cust2.setCustomerId(2);
-
-        CustomerCollectionsDAO CustDAO = new CustomerCollectionsDAO();
-        CustDAO.saveCustomer(cust1);
-
-        //Use of assert equals to check that get customer method, actually retrieves customer 1 from list based of username.
-        //This test also confirms our save function is working successfully
-        Customer retrieved = CustDAO.getCustomer("bayta267");
-
-        assertEquals("Retreived customer should be the same", cust1, retrieved);
-        /**
-         * assertTrue("cust1 should exist", CustDAO.contains(cust1));
-         */
-        
-        
-        //Follow up testing of save and get function testing manual attributes
-        CustDAO.saveCustomer(cust2);
-        Customer secondRetreieved = CustDAO.getCustomer("a267");
-
-        //Manual checks of each function
-        assertEquals(cust2.getFirstName(), secondRetreieved.getFirstName());
-        assertEquals(cust2.getLastName(), secondRetreieved.getLastName());
-        assertEquals(cust2.getUsername(), secondRetreieved.getUsername());
-        assertEquals(cust2.getPassword(), secondRetreieved.getPassword());
-        assertEquals(cust2.getPhoneNumber(), secondRetreieved.getPhoneNumber());
-        assertEquals(cust2.getEmailAddress(), secondRetreieved.getEmailAddress());
-        assertEquals(cust2.getCustomerId(), secondRetreieved.getCustomerId());
-
-        //credential validation test
-        assertTrue(CustDAO.validateCredentials("a267", "1234"));
-        assertFalse(CustDAO.validateCredentials("Taine", "Bayly"));
-    }
-
-    @Test
-    public void testSaveSubscription() {
-        Subscription sub1 = new Subscription();
-        sub1.setName("Netflix");
-        sub1.setSubscriptionId(123);
-        sub1.setPaid(false);
-        sub1.setCategory("Leisure");
-        sub1.setSubscriptionPrice(BigDecimal.TEN);
-        sub1.setDescription("Movies and TV");
-        sub1.setCompanyName("Netflix Inc.");
-
-        Subscription sub2 = new Subscription();
-        sub2.setName("flix");
-        sub2.setSubscriptionId(1234);
-        sub2.setPaid(false);
-        sub2.setCategory("Lei");
-        sub2.setSubscriptionPrice(BigDecimal.TEN);
-        sub2.setDescription("TV");
-        sub2.setCompanyName("flix Inc.");
-
-        SubscriptionCollectionsDAO subDAO = new SubscriptionCollectionsDAO();
-
-        subDAO.saveSubscription("subtest1", sub1);
-
-        //below sits tests that should work if we adjust subscriptionDAO get method
-        
-        Collection<Subscription> subsRetrieved = subDAO.getSubscriptionsByUsername("subtest1");
-        
-        assertThat(subsRetrieved, hasItem(sub1));
-        
-        //Manual checks of each function attribute success for sub2
-        subDAO.saveSubscription("subtest2",sub2);
-        /*
-        Subscription subRetrieved2 = subDAO.getSubscriptionsByUsername("Subtest2");
-        assertEquals(sub2.getName(), subRetrieved2.getName());
-        assertEquals(sub2.getSubscriptionId(), subRetrieved2.getgetSubscriptionId));
-        assertEquals(sub2.getPaid(), subRetrieved2.getPaid());
-         assertEquals(sub2.getCategory(), subRetrieved2.getCategory());
-        assertEquals(sub2.getSubscriptionPrice(), subRetrieved2.getSubscriptionPrice());
-        assertEquals(sub2.getDescription(), subRetrieved2.getDescription());
-        assertEquals(sub2.getCompanyName(), subRetrieved2.getCompanyName());
-        
-         */
-        // assertThat(1 + 1, equalTo(2));
+    public void testSomething() {
+        assertThat(1 + 1, equalTo(2));
     }
 
 }
