7bc086f69beb8bbb74cad9b4e76d137fd634961e
emmabrothers <emmabrothers@localhost>
2021-04-26 11:04:55 +1200

    Subscription Module Class added Get subscription by username method References #9

--- src/main/java/web/SubscriptionModule.java
+++ src/main/java/web/SubscriptionModule.java
@@ -11,7 +11,35 @@ import org.jooby.Jooby;
  *
  * @author emmabrothers
  */
+
+import dao.SubscriptionDAO;
+import domain.Customer;
+import org.jooby.Jooby;
+import org.jooby.Result;
+import org.jooby.Status;
+
+/**
+ *
+ * @author emmabrothers
+ */
 public class SubscriptionModule extends Jooby{
-    
+   // saveSubscription
+   // getSubscriptionsByUsername
+    public SubscriptionModule(SubscriptionDAO subscriptionDao){
+        get("/api/subscription/:username", (req) -> {
+            String username = req.param("username").value();
+            if(subscriptionDao.getSubscriptionsByUsername(username) == null){
+                return new Result().status(Status.NOT_FOUND);
+            }else{
+                return subscriptionDao.getSubscriptionsByUsername(username);
+            }
+        });
+        
+            
+    // used similar get method as CustomerModule.java as they both were 
     
 }
+}
+    
+    
+
