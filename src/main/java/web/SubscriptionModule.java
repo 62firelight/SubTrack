0ff93517fa15453223eda1e7a71990834d18c382
Luke Tang <tanlu824@student.otago.ac.nz>
2021-05-11 17:46:44 +1200

    Auto indented with NetBeans
    References #43.

--- src/main/java/web/SubscriptionModule.java
+++ src/main/java/web/SubscriptionModule.java
@@ -5,12 +5,10 @@
  */
 package web;
 
-
 /**
  *
  * @author emmabrothers
  */
-
 import dao.SubscriptionDAO;
 import domain.Subscription;
 import java.time.LocalDate;
@@ -22,32 +20,29 @@ import org.jooby.Status;
  *
  * @author emmabrothers
  */
-public class SubscriptionModule extends Jooby{
-   // saveSubscription
-   // getSubscriptionsByUsername
-    public SubscriptionModule(SubscriptionDAO subscriptionDao){
+public class SubscriptionModule extends Jooby {
+    // saveSubscription
+    // getSubscriptionsByUsername
+
+    public SubscriptionModule(SubscriptionDAO subscriptionDao) {
         get("/api/subscriptions/:username", (req) -> {
             String username = req.param("username").value();
-            if(subscriptionDao.getSubscriptionsByUsername(username) == null){
+            if (subscriptionDao.getSubscriptionsByUsername(username) == null) {
                 return new Result().status(Status.NOT_FOUND);
-            }else{
+            } else {
                 return subscriptionDao.getSubscriptionsByUsername(username);
             }
         });
-        
-            
-     post("/api/subscriptions", (req, rsp) -> {
+
+        post("/api/subscriptions", (req, rsp) -> {
             Subscription subscription = req.body().to(Subscription.class);
-            
+
             LocalDate dueDate = LocalDate.parse(subscription.getDueDate().substring(0, 10));
             System.out.println(dueDate);
             subscription.setDueDate(dueDate.toString());
-            
+
             subscriptionDao.saveSubscription(subscription);
             rsp.status(Status.CREATED);
         });
+    }
 }
-}
-    
-    
-
