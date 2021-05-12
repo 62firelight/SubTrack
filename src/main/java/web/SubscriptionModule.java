25c326912c6477e5f018089edec69125d844a49a
Taine Bayly <bayta257@student.otago.ac.nz>
2021-05-13 09:37:46 +1200

    implementation of email

--- src/main/java/web/SubscriptionModule.java
+++ src/main/java/web/SubscriptionModule.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package web;
 
 /**
@@ -10,9 +5,19 @@ package web;
  * @author emmabrothers
  */
 import dao.SubscriptionDAO;
+import domain.Customer;
 import domain.Subscription;
 import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import static java.time.LocalDate.parse;
+import java.util.Date;
+import java.util.concurrent.CompletableFuture;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
 import org.jooby.Jooby;
 import org.jooby.Result;
 import org.jooby.Status;
@@ -41,7 +46,7 @@ public class SubscriptionModule extends Jooby {
             LocalDate dueDate = LocalDate.parse(subscription.getDueDate().substring(0, 10));
             System.out.println(dueDate);
             subscription.setDueDate(dueDate.toString());
-            
+
             // decide whether a subscription is paid or not based on price
             if (subscription.getSubscriptionPrice().equals(BigDecimal.ZERO)) {
                 subscription.setPaid(false);
@@ -49,17 +54,35 @@ public class SubscriptionModule extends Jooby {
 
             subscriptionDao.saveSubscription(subscription);
             rsp.status(Status.CREATED);
+
+            Customer c = subscription.getCustomer();
+            String customerEmail = c.getEmailAddress();
+            String fName = c.getFirstName();
+            String lName = c.getLastName();
+            String date = subscription.getDueDate();
+
+            CompletableFuture.runAsync(() -> {
+                try {
+                    Email email = new SimpleEmail();
+                    email.setHostName("localhost");
+                    email.setSmtpPort(2525);
+                    email.setFrom("trbayly145@gmail.com");
+                    email.setSubject("Subscription Renewal Warning");
+                    email.setMsg("Customer: " + fName + " " + lName + "\n" + " This is an email to warn you that your subscription expires Date: " + date);
+                    //email.setMsg("hey");
+                    email.addTo(customerEmail);
+                    //email.addTo("foo@bar.com");
+                    email.send();
+                } catch (EmailException ex) {
+                    Logger.getLogger(SubscriptionModule.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            });
         });
-     
-     get("api/categories", ()->subscriptionDao.getCategories());
-     get("api/categories/:cateogires",(req)->{
-         String category = req.param("category").value();
-            return subscriptionDao.filterByCategory(category);
-     });
-     delete("/api/subscriptions/:id", (req, rsp) -> {
+
+        delete("/api/subscriptions/:id", (req, rsp) -> {
             Integer id = Integer.valueOf(req.param("id").value());
             Subscription subscription = subscriptionDao.getSubscriptionById(id);
-            
+
             subscriptionDao.deleteSubscription(subscription);
             rsp.status(Status.NO_CONTENT);
         });
