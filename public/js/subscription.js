c5e1c79bf62f2b942b8527c3faac6a5ad1b1be76
Luke Tang <tanlu824@student.otago.ac.nz>
2022-08-08 17:29:29 +1200

    Re-added ability to see subscriptions

--- public/js/subscription.js
+++ public/js/subscription.js
@@ -17,68 +17,87 @@ var deleteSubApi = ({id}) => `api/subscriptions/${id}`;
 var categoryApi = ({username}) => `api/categories/${username}`;
 var sortApi = ({username}) => `api/sort/${username}`;
 var filterApi = ({category}, {username}) => `api/categories/${category}/${username}`;
-var totalApi =  ({username}) => `api/total/${username}`;
+var totalApi = ({username}) => `api/total/${username}`;
 
 const app = Vue.createApp({
-   
+
     data() {
         return {
-            welcome: `Welcome. The current date is ${(new Date()).toLocaleDateString()}.`
+            welcome: `Welcome. The current date is ${(new Date()).toLocaleDateString()}.`,
+            subscriptions: new Array()
         }
     },
-    
+
+    computed: {
+        signedIn() {
+            return this.customer != null;
+        },
+        ...Vuex.mapState({
+                customer: 'customer'
+        })
+    },
+
     mounted() {
-        
-    },
-    
-    methods: {
-        
-        addSub(subscription) {
-            
-        },
-        
-        getSubs(username) {
-            
-        },
-        
-        deleteSub(subscription) {
-            
-        },
-        
-        updateSub(subscription) {
-            
-        },
-        
-        renewSub(subscription) {
-            
-        },
-        
-        getSubStatus(subscription) {
-            
-        },
-        
-        getConvertedDate(date) {
-            
-        },
-        
-        daysToToday(dateString) {
-            
-        },
-        
-        filterCat(selectedCat) {
-            
-        },
-        
-        selectAll() {
-            
-        },
-        
-        sort() {
-            
+        if (this.signedIn) {
+            this.getSubs();
         }
-        
+    },
+
+    methods: {
+
+        addSub(subscription) {
+
+        },
+
+        getSubs() {
+            axios.get(subApi({'username': this.customer.username}))
+                    .then(response => {
+                        this.subscriptions = response.data;
+                    })
+                    .catch(error => {
+                        console.log(error);
+                        alert('An error has occurred - check the console for details');
+                    })
+        },
+
+        deleteSub(subscription) {
+
+        },
+
+        updateSub(subscription) {
+
+        },
+
+        renewSub(subscription) {
+
+        },
+
+        getSubStatus(subscription) {
+
+        },
+
+        getConvertedDate(date) {
+
+        },
+
+        daysToToday(dateString) {
+
+        },
+
+        filterCat(selectedCat) {
+
+        },
+
+        selectAll() {
+
+        },
+
+        sort() {
+
+        }
+
     }
-    
+
 });
 
 import { dataStore } from './data-store.js';
