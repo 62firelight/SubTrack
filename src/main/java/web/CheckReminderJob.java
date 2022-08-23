5818f909fb3ff43c1ec5f106484abd042994a3be
Luke Tang <tanlu824@student.otago.ac.nz>
2022-08-23 14:09:25 +1200

    Check if sub is expired
    References #4.

--- src/main/java/web/CheckReminderJob.java
+++ src/main/java/web/CheckReminderJob.java
@@ -8,9 +8,11 @@ package web;
 import dao.SubscriptionDAO;
 import domain.Subscription;
 import io.jooby.quartz.Scheduled;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
+import java.util.Collection;
 
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
@@ -27,12 +29,23 @@ public class CheckReminderJob implements Job {
     }
     
     @Override
-    @Scheduled("5s")
+    @Scheduled("1m")
     public void execute(JobExecutionContext jec) throws JobExecutionException {
         LocalDateTime currentDateTime = LocalDateTime.now();
         String time = currentDateTime.format(DateTimeFormatter.ofLocalizedTime(FormatStyle.MEDIUM));
         
-        System.out.println(time + " - There are " + this.subscriptionDao.getSubscriptions().size() + " subscription(s).");
+        Collection<Subscription> subs = this.subscriptionDao.getSubscriptions();
+        
+        LocalDate currentDate = currentDateTime.toLocalDate();
+        for (Subscription sub : subs) {
+            LocalDate dueDate = LocalDate.parse(sub.getDueDate());
+            
+            if (currentDate.compareTo(dueDate) >= 0) {
+                System.out.println(sub.getName() + " is expired");
+            }
+        }
+        
+//        System.out.println(time + " - There are " + subs.size() + " subscription(s).");
     }
     
 }
